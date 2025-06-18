d25b896463a454648f14ed980449873168d39168
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2025-06-18 20:01:34 +0400

    update

--- client.c
+++ client.c
@@ -6,25 +6,47 @@
 /*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 19:18:56 by aramarak          #+#    #+#             */
-/*   Updated: 2025/06/16 20:04:09 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/06/18 19:36:24 by aramarak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	send_char(pid_t pid, char c)
+volatile sig_atomic_t g_ack = 0;
+
+// void	send_char(pid_t pid, char c)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (i < 8)
+// 	{
+// 		if ((c >> i) & 1)
+// 			kill(pid, SIGUSR2);
+// 		else
+// 			kill(pid, SIGUSR1);
+// 		usleep(300);
+// 		i++;
+// 	}
+// }
+
+/**
+ * @file client.c
+ * 
+ * @brief	Client-side code for sending characters to a server process via signals.
+ * @param	server_pid The process ID of the server.
+ * @param	c The character to be sent
+ * @return	void
+ */
+void	ft_send_char(pid_t server_pid, char c)
 {
 	int	i;
 
 	i = 0;
 	while (i < 8)
 	{
-		if ((c >> i) & 1)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		usleep(300);
-		i++;
+		ft_send_bit(server_pid, (c >> 1) & 1);
+		++i;
 	}
 }
 
